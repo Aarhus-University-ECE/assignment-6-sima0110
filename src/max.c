c2217a70bea724abdff8388cc4385cea456db754
sima0110 <simajad.sj@gmail.com>
2022-10-06 18:42:22 +0200

    done

--- src/max.c
+++ src/max.c
@@ -2,9 +2,13 @@
 #include <assert.h>
 
 int max(int* numbers, int size)
-{
-    // Excercise 2
-    // Implement your code below...
-
-    return -1;
+{ 
+assert(size > 0); //checks the precondition//
+int max = number[0] //considers the first elemenent as "max"//
+for (int i = 0; i < size; i++){
+    if (numbers[i]> max){ //compares the other elements wirth "max"//
+        max = numbers[i]; //if an element is bigger than "max", it becomes the new max value//
+    }
+}
+    return max;
 }
